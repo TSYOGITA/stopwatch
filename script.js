ee32df10651c0cc11cdc10f8ea81f7957b45a377
TSYOGITA <77011543+TSYOGITA@users.noreply.github.com>
2021-01-08 12:24:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+var seconds = 00;
+var tens = 00;
+var appendTens = document.getElementById('tens');
+var appendSeconds = document.getElementById('seconds');
+var buttonStart = document.getElementById('start-btn');
+var buttonStop = document.getElementById('stop-btn');
+var buttonRestart = document.getElementById('restart-btn');
+var interval;
+
+function startTimer(){
+    tens++;
+
+    if (tens < 9) {
+        appendTens.innerHTML = "0" + tens;
+    }
+    if (tens > 9) {
+        appendTens.innerHTML = tens;
+    }
+    if (tens > 99) {
+        seconds++;
+        appendSeconds.innerHTML = "0" + seconds;
+        tens = 0;
+        appendTens.innerHTML = "0" + 0;
+    }
+    if (seconds > 9) {
+        appendSeconds.innerHTML =  seconds;
+    }
+}
+
+buttonStart.onclick = function(){
+    interval = setInterval(startTimer);
+};
+
+buttonStop.onclick = function(){
+    clearInterval(interval);
+};
+
+buttonRestart.onclick = function(){
+    clearInterval(interval);
+    tens = "00";
+    seconds = "00";
+    appendSeconds.innerHTML = seconds;
+    appendTens.innerHTML = tens;
+};
